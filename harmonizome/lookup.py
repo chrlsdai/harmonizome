cc0d0464564ab7e5527ac562420ac636160c2182
Charles Dai <crd232@cornell.edu>
2020-07-02 13:54:00 -0400

    added ensembl lookup feature

--- harmonizome/lookup.py
+++ harmonizome/lookup.py
@@ -5,18 +5,31 @@ from tqdm import tqdm
 import numpy as np
 import datetime
 
+'''
+Sources are currently human, mouse, and rat, in order of descending priority.
+'''
+sources = [
+    'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Rattus_norvegicus.gene_info.gz',
+    'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Mus_musculus.gene_info.gz',
+    'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz'
+]
 
-def get_dictionary(path):
+
+def get_dictionary(path, mapfrom):
     '''
     Returns two dictionaries, the first a mapping from symbols to approved gene
     symbols (synonyms), the second a mapping from approved symbols to Entrez
     Gene IDs from an NCBI gene info source designated by path.
     '''
+    column = {
+        'synonyms': 'Synonyms',
+        'ensembl': 'dbXrefs'
+    }[mapfrom]
     ncbi = pd.read_csv(path, sep='\t', usecols=[
-                       'Symbol', 'Synonyms', 'GeneID'])
+        'Symbol', 'GeneID', column])
 
     def split_list(v): return v.split('|') if type(v) == str else []
-    ncbi['Synonyms'] = ncbi['Synonyms'].apply(split_list)
+    ncbi[column] = ncbi[column].apply(split_list)
 
     # Map existing entities to NCBI Genes
     symbol_lookup = {}
@@ -26,35 +39,30 @@ def get_dictionary(path):
         v = ncbi.loc[i, 'GeneID']
         geneid_lookup[approved_sym] = v if v != '-' else np.nan
 
-        for sym in [approved_sym] + ncbi.loc[i, 'Synonyms']:
-            if not (sym == '-'):
-                symbol_lookup[sym] = approved_sym
+        if mapfrom == 'synonyms':
+            for sym in [approved_sym] + ncbi.loc[i, column]:
+                if not (sym == '-'):
+                    symbol_lookup[sym] = approved_sym
+        elif mapfrom == 'ensembl':
+            for sym in ncbi.loc[i, column]:
+                if sym.startswith('Ensembl:'):
+                    symbol_lookup[sym[len('Ensembl:'):]] = approved_sym
 
     return symbol_lookup, geneid_lookup
 
 
-def get_lookups():
+def get_lookups(mapfrom='synonyms'):
     '''
     Returns two dictionaries, the first a mapping from symbols to approved gene
     symbols (synonyms), the second a mapping from approved symbols to Entrez
     Gene IDs.
 
-    Sources are currently human, mouse, and rat, in order of descending priority.
     '''
-    sources = [
-        'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Rattus_norvegicus.gene_info.gz',
-        'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Mus_musculus.gene_info.gz',
-        'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz'
-    ]
-    # sources = [
-    #     'ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/All_Data.gene_info.gz'
-    # ]
-
     symbol_lookup = {}
     geneid_lookup = {}
 
     for source in tqdm(sources, desc='Gathering sources'):
-        sym, gene = get_dictionary(source)
+        sym, gene = get_dictionary(source, mapfrom)
         symbol_lookup.update(sym)
         geneid_lookup.update(gene)
 
